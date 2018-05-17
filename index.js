1ee595cd236928e43771dfa9639577cbb58f1d51
Tianchi Xu <xtc459924276@gmail.com>
2018-05-16 20:09:58 -0400

    add a delete button to each list item

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const form = document.querySelector('form')
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    f = e.target
-    console.log(f.item.value);
+const listArray = []
+
+form.addEventListener('submit',(e)=>{    
+    e.preventDefault()
+    itemName = e.target.item.value
+    listArray.push(itemName)
+
+    const listItem = document.createElement('li')
+    listItem.textContent = itemName
+    createDeleteBtn(listItem)
+
+    const list = document.querySelector("#list")
+    list.appendChild(listItem)
+
+    console.log(listArray)
+    e.target.reset()
+    e.target.item.focus()
 })
 
-const list = document.createElement('ul');
+function createDeleteBtn(listItem){
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Delete'
+    deleteBtn.addEventListener('click',()=>{
+        listItem.parentNode.removeChild(listItem)
+    })
+    listItem.appendChild(deleteBtn)
+}
